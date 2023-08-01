5be6b9112be51b1a645b9410a3cad2edc34f58ed
sumon <18103159@iubat.edu>
2023-08-01 13:59:39 +0600

    db comment out

--- database/migrations/2014_10_12_100000_create_password_reset_tokens_table.php
+++ database/migrations/2014_10_12_100000_create_password_reset_tokens_table.php
@@ -4,11 +4,8 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
+return new class extends Migration{
+
     public function up(): void
     {
         Schema::create('password_reset_tokens', function (Blueprint $table) {
@@ -18,11 +15,8 @@ return new class extends Migration
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
+
+    public function down(): void{
         Schema::dropIfExists('password_reset_tokens');
     }
 };
